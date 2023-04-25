00ea66a3340e62dff82f34da8b06d10f92fbb8fd
Dobrin Ionut <91891857+ionutdd@users.noreply.github.com>
2023-04-25 22:18:13 +0300

    Update Backtracking.cpp

--- Probleme_clasice/Backtracking.cpp
+++ Probleme_clasice/Backtracking.cpp
@@ -30,3 +30,12 @@ int main() {
     backtrack(n, k, 1, combination);
     return 0;
 }
+
+
+/*
+Funcția backtrack primește ca argumente n - numărul de elemente din set, k - numărul de elemente din fiecare combinație, start - index-ul de început pentru iterarea prin set și combination - vectorul în care se stochează combinația curentă.
+În interiorul funcției, avem un caz de bază, în care verificăm dacă combinația curentă are k elemente și, în caz afirmativ, o afișăm.
+În continuare, încercăm toate combinațiile posibile prin a adăuga fiecare element la combinația curentă și a apela recursiv funcția cu start setat la indexul următor.
+Apoi, eliminăm ultimul element adăugat pentru a încerca următorul.
+În funcția main, apelăm funcția backtrack cu valorile n, k și vectorul combinatii gol pentru a obține toate combinațiile de k elemente din setul de n elemente.
+*/
